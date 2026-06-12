272d89779ce613e81406a5379192cfbdaf1da95f
Andrei <andrei@example.com>
2026-06-12 14:36:35 +0200

    fix script

--- linux_security/network_protocols/0-iptables.sh
+++ linux_security/network_protocols/0-iptables.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-# Ensure the script is being run with root privileges
-
-if [ "$EUID" -ne 0 ]; then
-
-   echo "Error: This script must be run as root. Please use 'sudo'."
-
-   exit 1
-
-fi
-
-# Display all iptables rules with verbose stats and line numbers
-
 iptables -vnL --line-numbers
