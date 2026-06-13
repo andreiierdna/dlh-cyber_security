fbd95e95346bd9b7f1bd77ab058facc2dcdb5497
Andrei <andreiierdna>
2026-06-13 23:02:13 +0200

    8-smtp.sh

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-postconf -h smtpd_tls_security_level smtpd_use_tls 2>/dev/null | grep -qiE '^smtpd_tls_security_level$|^(encrypt|yes|may)$' || echo "STARTTLS not configured"
+postconf | grep "^smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
