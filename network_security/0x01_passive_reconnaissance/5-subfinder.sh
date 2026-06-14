2c9d8351a829b48011447e06d936c1e84c66c945
Andrei <andreiierdna>
2026-06-14 23:18:22 +0200

    5-subfinder.sh simpler

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | tee /dev/tty | xargs -I {} sh -c 'ip=$(dig +short "{}" | tail -n1); [ -n "$ip" ] && echo "{},$ip"' > "$1.txt"
+subfinder -silent -d $1 -o $1.txt -nW -oI
